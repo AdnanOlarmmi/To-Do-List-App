e9855482eeba71ed904951d2463e0f75c80c77eb
AdnanOlarmmi <olarmmi@gmail.com>
2022-09-28 18:38:41 +0100

    Make the Add, Remove and Edit Work Effectively

---
+++ src/modules/addTask.js
@@ -0,0 +1,29 @@
+import renderToDoItems from './renderTasks.js';
+import toDoItemsArray from './toDoItemsArray.js';
+
+const addElBtn = document.querySelector('.to-do__addTask');
+const todoinputEl = document.querySelector('.to-do__input');
+const input = document.querySelector('input');
+
+const addTask = () => {
+  addElBtn.addEventListener('click', () => {
+    const newTask = todoinputEl.value;
+    if (newTask) {
+      const newTodo = {
+        description: newTask,
+        completed: false,
+      };
+      toDoItemsArray.push(newTodo);
+      localStorage.setItem('toDoItemsArray', JSON.stringify(toDoItemsArray));
+      renderToDoItems();
+      todoinputEl.value = '';
+    } else {
+      input.classList.add('inputError');
+      setTimeout(() => {
+        input.classList.remove('inputError');
+      }, 1000);
+    }
+  });
+};
+
+export default addTask;
