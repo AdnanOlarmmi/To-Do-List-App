815060d8013b11a7981885ab274629203077d767
derrick <djmulinde17@gmail.com>
2022-10-06 18:26:28 +0300

    Add clear completed task  test

--- edit_clear_completed.test.js
+++ edit_clear_completed.test.js
@@ -2,6 +2,7 @@ import addTask from './src/modules/addTask.js';
 import toDoItemsArray from './src/modules/toDoItemsArray.js';
 import editT from './src/modules/edit.js';
 import completed from './src/modules/updateCompleted.js';
+import clearCompleted from './src/modules/clearCompleted.js';
 
 describe('test edit and clear completed', () => {
   test('test edit task function', () => {
@@ -14,5 +15,11 @@ describe('test edit and clear completed', () => {
   test('test edit task function', () => {
     completed(0, true);
     expect(toDoItemsArray[0].description).toBe(true);
+  });
+
+  test('test clear completed task function', () => {
+    toDoItemsArray[0].completed = true;
+    clearCompleted();
+    expect(toDoItemsArray).toHaveLength(1);
   });
 });
