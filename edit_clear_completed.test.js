b931a28737388ae5ff4c8bc19873d826dc0362a3
derrick <djmulinde17@gmail.com>
2022-10-06 18:25:32 +0300

    Fix eslint error

--- edit_clear_completed.test.js
+++ edit_clear_completed.test.js
@@ -1,13 +1,12 @@
 import addTask from './src/modules/addTask.js';
 import toDoItemsArray from './src/modules/toDoItemsArray.js';
-import clearCompleted from './src/modules/clearCompleted.js';
 import editT from './src/modules/edit.js';
 import completed from './src/modules/updateCompleted.js';
 
-describe('test edit and clearcompleted', () => {
+describe('test edit and clear completed', () => {
   test('test edit task function', () => {
-    addTask('first task');
-    addTask('second task');
+    addTask('First task');
+    addTask('Second task');
     editT('changed', 0);
     expect(toDoItemsArray[0].description).toBe('changed');
   });
@@ -15,11 +14,5 @@ describe('test edit and clearcompleted', () => {
   test('test edit task function', () => {
     completed(0, true);
     expect(toDoItemsArray[0].description).toBe(true);
-  });
-
-  test('test clear completed task function', () => {
-    toDoItemsArray[0].completed = true;
-    clearCompleted();
-    expect(toDoItemsArray).toHaveLength(1);
   });
 });
